7d22685518c619fea7b972bf8455cb193ff9fe71
Bharathi.Valluri <you@example.com>
2022-11-30 14:39:48 +0530

    modified

--- source/routes/routes.ts
+++ source/routes/routes.ts
@@ -4,9 +4,7 @@ const router = express.Router()
 
 router.post('/saveData', user.add)
 router.get('/find', user.find)
-// router.get('/posts/:id', userController.getPost);
 router.put('/updating', user.updateRecord)
 router.delete('/remove', user.deleteRecord)
-// router.post('/posts', controller.addPost);
 
 export { router }
